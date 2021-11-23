a9682c0b5ffabb700631de4e81e3567901845a98
Rafa90k <embau12rafael@gmail.com>
2021-11-23 18:32:43 -0300

    first commit

---
+++ ProgramaJava.java
@@ -0,0 +1,19 @@
+package Git.dio;
+
+import Git.dio.Gato.Gato;
+
+public class ProgramaJava {
+
+	public static void main(String[] args) {
+	
+	Gato gato = new Gato();
+	
+	System.out.println(gato);
+		
+		
+
+	}
+
+
+}
+
